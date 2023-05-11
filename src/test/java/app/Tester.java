6e0dd8a318f5f884ca682b457056e41dda529876
Ahmed Hussein <ahmadqasemtech11@gmail.com>
2023-05-11 20:26:33 +0300

    update1

--- src/test/java/app/Tester.java
+++ src/test/java/app/Tester.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:addOrder.feature",
+@CucumberOptions(features = "classpath:.feature",
                 plugin = "html:target/cucumber/wikipedia.html"
 )
 public class Tester {
