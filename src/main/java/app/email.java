102a4ef58ffbfdfb8f17151b717c147e40e2e212
Ahmed Hussein <ahmadqasemtech11@gmail.com>
2023-05-14 02:53:47 +0300

    update13

--- src/main/java/app/email.java
+++ src/main/java/app/email.java
@@ -8,7 +8,7 @@ import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
+
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 public class email {
